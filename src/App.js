17a528c4710575b4088a3e643e5efd66844fbfa8
Craig Johnson <craig.johnson@helpshift.com>
2021-03-30 15:59:59 +0530

    search added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import styled from 'styled-components'
 import * as icons from './icons'
 import Header from './components/Header'
 import IconWrapper from './components/IconWrapper'
+import { useState } from 'react';
+
 // import {NotificationOffeDuotoneF} from './icons'
-
-
 
 const Container = styled.ul`
   display: grid;
@@ -18,29 +18,60 @@ const Container = styled.ul`
   font-family: sans-serif;
 `
 
-class List extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header/>
-        <Container>
-          {/* <NotificationOffeDuotoneF/> */}
-          {
-            Object.keys(icons)
-              .map((key, index) => {
-                const Icon = icons[key]
-                return <li key={index}>
-                  <IconWrapper>
-                    <Icon size="60" color="#333"/>
-                    <span>{key}</span>
-                  </IconWrapper>
-                </li>
-              })
-          }
-        </Container>
-      </div>
-    )
+const Search = styled.input`
+display: block;
+width: 30%;
+line-height: 40px;
+border: 3px solid #444;
+border-radius: 7px;
+margin: 32px auto;
+font-family: sans-serif;
+text-indent: 10px;
+`
+
+function List() {
+
+  const iconArray = Object.keys(icons)
+    .map((key, index) => {
+      const Icon = icons[key]
+      return {
+        name: key,
+        icon: Icon
+      }
+    })
+
+  const [iconsList, setIconList] = useState(iconArray);
+
+  const filterIconList = (e) => {
+
+    let filteredIconList = iconArray.filter(({ name }) => {
+      return name.toLowerCase().includes(e.target.value.toLowerCase())
+    });
+
+    setIconList(filteredIconList)
   }
+
+  return (
+    <div>
+      <Header/>
+      <Search type="text" onChange={filterIconList} placeholder="Search"/>
+      <Container>
+        {/* <NotificationOffeDuotoneF/> */}
+        {
+          iconsList.map(({icon, name}, index) => {
+              const Icon = icon;
+
+              return <li key={index}>
+                <IconWrapper>
+                  <Icon size="60" color="#333"/>
+                  <span>{name}</span>
+                </IconWrapper>
+              </li>
+            })
+        }
+      </Container>
+    </div>
+  )
 }
 
 export default List;
