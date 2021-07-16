440134d1cf254dde71fce836d0655d64690dd507
convertlab-cl <87417854+convertlab-cl@users.noreply.github.com>
2021-07-16 19:36:24 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class List extends React.Component {
                 const Icon = icons[key]
                 return <li key={index}>
                   <IconWrapper>
-                    <Icon/>
+                    <Icon style={{width: 32, height: 32}}/>
                     <span>{key}</span>
                   </IconWrapper>
                 </li>
