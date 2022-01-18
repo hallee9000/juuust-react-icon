1302246367738b0f06cb1424b80a57fef77bd6c9
JsCoder <17317112520@163.com>
2022-01-18 17:30:25 +0800

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,10 @@ import styled from 'styled-components';
 
 const Header = styled.header`
   display: block;
+  text-align: 'center';
 `
 
 export default () =>
   <Header>
-    <h1>Beautiful Icons</h1>
+    <h1>TA Beautiful Icons</h1>
   </Header>
