ad111c9050ff4c9ce5d8ae118205c3017ccd65a3
JsCoder <17317112520@163.com>
2022-02-23 15:27:02 +0800

    fix: icon viewBox size

--- bin/template.js
+++ bin/template.js
@@ -3,7 +3,7 @@ const getAttrs = (style) => {
     xmlns: 'http://www.w3.org/2000/svg',
     width: 'size',
     height: 'size',
-    viewBox: '0 0 16 16',
+    viewBox: '0 0 24 24',
   }
   const fillAttrs = {
     fill: 'color',
