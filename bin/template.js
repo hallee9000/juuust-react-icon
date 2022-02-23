95776c7b357a2877c4a1c4df9e0bb01dc1cb1170
JsCoder <17317112520@163.com>
2022-02-23 15:17:19 +0800

    feat: set icon default size = 16

--- bin/template.js
+++ bin/template.js
@@ -3,7 +3,7 @@ const getAttrs = (style) => {
     xmlns: 'http://www.w3.org/2000/svg',
     width: 'size',
     height: 'size',
-    viewBox: '0 0 24 24',
+    viewBox: '0 0 16 16',
   }
   const fillAttrs = {
     fill: 'color',
@@ -43,7 +43,7 @@ const getElementCode = (ComponentName, attrs, svgCode) => `
 
   ${ComponentName}.defaultProps = {
     color: 'currentColor',
-    size: '24',
+    size: '16',
   }
 
   export default ${ComponentName}
