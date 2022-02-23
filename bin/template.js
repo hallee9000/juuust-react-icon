952bec346992faf9af12b56504b45c8f64874c6c
JsCoder <17317112520@163.com>
2022-02-23 19:10:59 +0800

    fix: sm icon set viewBox 16

--- bin/template.js
+++ bin/template.js
@@ -1,9 +1,9 @@
-const getAttrs = (style) => {
+const getAttrs = (style, isSm = false) => {
   const baseAttrs = {
     xmlns: 'http://www.w3.org/2000/svg',
     width: 'size',
     height: 'size',
-    viewBox: '0 0 24 24',
+    viewBox: isSm ? '0 0 16 16' : '0 0 24 24',
   }
   const fillAttrs = {
     fill: 'color',
