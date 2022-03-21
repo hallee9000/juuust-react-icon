f7d602ef6c8203de34761cce50a9033c547844db
黄松松 <huangsongsong@thinkingdata.cn>
2022-03-21 17:43:39 +0800

    feat: add spin property

--- bin/template.js
+++ bin/template.js
@@ -1,42 +1,56 @@
 const getAttrs = (style, isSm = false) => {
   const baseAttrs = {
-    xmlns: 'http://www.w3.org/2000/svg',
-    width: 'size',
-    height: 'size',
-    viewBox: isSm ? '0 0 16 16' : '0 0 24 24',
-  }
+    xmlns: "http://www.w3.org/2000/svg",
+    width: "size",
+    height: "size",
+    viewBox: isSm ? "0 0 16 16" : "0 0 24 24",
+  };
   const fillAttrs = {
-    fill: 'color',
-    otherProps: '...otherProps'
-  }
+    fill: "color",
+    otherProps: "...otherProps",
+  };
   const strokeAttrs = {
-    fill: 'none',
-    stroke: 'color',
+    fill: "none",
+    stroke: "color",
     strokeWidth: 2,
-    strokeLinecap: 'round',
-    strokeLinejoin: 'round',
-    otherProps: '...otherProps'
-  }
-  return Object.assign({}, baseAttrs, style==='fill' ? fillAttrs : strokeAttrs)
-}
+    strokeLinecap: "round",
+    strokeLinejoin: "round",
+    otherProps: "...otherProps",
+  };
+  return Object.assign(
+    {},
+    baseAttrs,
+    style === "fill" ? fillAttrs : strokeAttrs
+  );
+};
 
 const getElementCode = (ComponentName, attrs, svgCode) => `
   import React from 'react';
   import PropTypes from 'prop-types';
 
+  const loadingCircleStyle = '@keyframes loadingCircle { 100% { transform: rotate(360deg) }}';
+
   const ${ComponentName} = (props) => {
-    const { color, size, style, ...otherProps } = props;
-    return (
+    const { color, size, spin, style, ...otherProps } = props;
+    return <>
+      <style children={loadingCircleStyle} />
       <svg ${attrs} style={{
         ...style,
+        ...(spin ? {
+          animationDuration: '1s',
+          animationIterationCount: 'infinite',
+          animationName: 'loadingCircle',
+          animationTimingFunction: 'linear'
+        } : {}),
         cursor: 'pointer'
-      }} >
+        }} >
         ${svgCode}
       </svg>
-    )
+    </>
   };
 
   ${ComponentName}.propTypes = {
+    spin: PropTypes.boolean,
     color: PropTypes.string,
     size: PropTypes.oneOfType([
       PropTypes.string,
@@ -45,11 +59,12 @@ const getElementCode = (ComponentName, attrs, svgCode) => `
   }
 
   ${ComponentName}.defaultProps = {
+    spin: false,
     color: 'currentColor',
     size: '16',
   }
 
   export default ${ComponentName}
-`
+`;
 
-module.exports = { getAttrs, getElementCode }
+module.exports = { getAttrs, getElementCode };
