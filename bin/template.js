f3d19f09ecbd84014d89e8ecd910182d90756238
黄松松 <huangsongsong@thinkingdata.cn>
2022-03-29 21:11:21 +0800

    fix: template className append

--- bin/template.js
+++ bin/template.js
@@ -32,7 +32,7 @@ const getElementCode = (ComponentName, attrs, svgCode) => `
 
   const ${ComponentName} = (props) => {
     const { color, size, spin, style, className, ...otherProps } = props;
-    return <span className={className ? 'tant-icon-span' + className : 'tant-icon-span'}>
+    return <span className={className ? 'tant-icon-span ' + className : 'tant-icon-span'}>
       <style children={loadingCircleStyle} />
       <svg ${attrs} style={{
         ...style,
