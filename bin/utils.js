1f8f37d8a7ce87370099ba6d15c607c0fbd0cb06
Sindri Berg Sævarsson <sindri.saevarsson@bestseller.com>
2023-01-20 09:05:39 +0100

    More fixes

--- bin/utils.js
+++ bin/utils.js
@@ -4,7 +4,8 @@ const parseName = (name, defaultStyle) => {
   const nameSlices = name.split('-')
   const style = nameSlices[nameSlices.length - 1]
   // Remove the -Fill suffis from copmponent name after using it for setting the style
-  const componentName = nameSlices.slice(0, nameSlices.length - 1).join('-')
+  // And move the "Icon" to the end of the component name
+  const componentName = nameSlices.slice(1, nameSlices.length - 1).join('-') + 'Icon'
   return {
     name,
     componentName: upperCamelCase(componentName),
