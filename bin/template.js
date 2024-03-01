def9f830e23a1e35cd62f7d67451502dc71aed59
zhujianshan <tango@thinkingdata.cn>
2024-03-01 16:52:11 +0800

    update testid

--- bin/template.js
+++ bin/template.js
@@ -1,28 +1,28 @@
 const getAttrs = (style, isSm = false) => {
   const baseAttrs = {
-    xmlns: "http://www.w3.org/2000/svg",
-    width: "size",
-    height: "size",
-    viewBox: isSm ? "0 0 16 16" : "0 0 24 24",
-  };
+    xmlns: 'http://www.w3.org/2000/svg',
+    width: 'size',
+    height: 'size',
+    viewBox: isSm ? '0 0 16 16' : '0 0 24 24',
+  }
   const fillAttrs = {
-    fill: "color",
-    otherProps: "...otherProps",
-  };
+    fill: 'color',
+    otherProps: '...otherProps',
+  }
   const strokeAttrs = {
-    fill: "none",
-    stroke: "color",
+    fill: 'none',
+    stroke: 'color',
     strokeWidth: 2,
-    strokeLinecap: "round",
-    strokeLinejoin: "round",
-    otherProps: "...otherProps",
-  };
+    strokeLinecap: 'round',
+    strokeLinejoin: 'round',
+    otherProps: '...otherProps',
+  }
   return Object.assign(
     {},
     baseAttrs,
-    style === "fill" ? fillAttrs : strokeAttrs
-  );
-};
+    style === 'fill' ? fillAttrs : strokeAttrs
+  )
+}
 
 const getElementCode = (ComponentName, attrs, svgCode) => `
   import React from 'react';
@@ -36,6 +36,7 @@ const getElementCode = (ComponentName, attrs, svgCode) => `
       <style children={loadingCircleStyle} />
       <svg ${attrs}
         className={iconClassName}
+        data-testid={testid}
         style={{
           ...style,
           ...(spin ? {
@@ -69,6 +70,6 @@ const getElementCode = (ComponentName, attrs, svgCode) => `
   }
 
   export default ${ComponentName}
-`;
+`
 
-module.exports = { getAttrs, getElementCode };
+module.exports = { getAttrs, getElementCode }
